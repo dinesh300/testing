fd19f9a5ceb6be86efaa26eb835dd68c43706b06
dinesh300 <dineshssnvlsi@gmail.com>
2023-04-25 13:59:37 -0700

    third commit

--- src/main/java/org/ex/FirstJob.java
+++ src/main/java/org/ex/FirstJob.java
@@ -5,7 +5,7 @@ public class FirstJob {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Successfully Installed");
+		System.out.println("Automatic build Successfully Installed");
 	}
 
 }
